de15d822f8c7d35ae8e215093f9f95080b4f56df
Álvaro Fernández Rojas <noltari@gmail.com>
2023-06-08 09:41:03 +0200

    webserver: cope with missing parameters
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- aioairzone_cloud/webserver.py
+++ aioairzone_cloud/webserver.py
@@ -52,22 +52,37 @@ class WebServer:
 
     def update(self, data: dict[str, Any]) -> None:
         """Update WebServer data."""
-        self.connection_date = str(data[API_STATUS][API_CONNECTION_DATE])
-        self.disconnection_date = str(data[API_STATUS][API_DISCONNECTION_DATE])
-        self.firmware = str(data[API_CONFIG][API_WS_FW])
-        self.is_connected = bool(data[API_STATUS][API_IS_CONNECTED])
-        self.type = str(data[API_WS_TYPE])
-        self.wifi_channel = int(data[API_CONFIG][API_STAT_CHANNEL])
-        self.wifi_quality = int(data[API_STATUS][API_STAT_QUALITY])
-        self.wifi_rssi = int(data[API_STATUS][API_STAT_RSSI])
-        self.wifi_ssid = str(data[API_CONFIG][API_STAT_SSID])
 
-        if API_STAT_AP_MAC in data[API_CONFIG]:
-            self.wifi_mac = str(data[API_CONFIG][API_STAT_AP_MAC])
+        if API_WS_TYPE in data:
+            self.type = str(data[API_WS_TYPE])
+
+        ws_config = data.get(API_CONFIG)
+        if ws_config is not None:
+            if API_STAT_AP_MAC in ws_config:
+                self.wifi_mac = str(ws_config[API_STAT_AP_MAC])
+            if API_STAT_CHANNEL in ws_config:
+                self.wifi_channel = int(ws_config[API_STAT_CHANNEL])
+            if API_STAT_SSID in ws_config:
+                self.wifi_ssid = str(ws_config[API_STAT_SSID])
+            if API_WS_FW in ws_config:
+                self.firmware = str(ws_config[API_WS_FW])
+
+        ws_status = data.get(API_STATUS)
+        if ws_status is not None:
+            if API_CONNECTION_DATE in ws_status:
+                self.connection_date = str(ws_status[API_CONNECTION_DATE])
+            if API_DISCONNECTION_DATE in ws_status:
+                self.disconnection_date = str(ws_status[API_DISCONNECTION_DATE])
+            if API_IS_CONNECTED in ws_status:
+                self.is_connected = bool(ws_status[API_IS_CONNECTED])
+            if API_STAT_QUALITY in ws_status:
+                self.wifi_quality = int(ws_status[API_STAT_QUALITY])
+            if API_STAT_RSSI in ws_status:
+                self.wifi_rssi = int(ws_status[API_STAT_RSSI])
 
     def data(self) -> dict[str, Any]:
         """Return WebServer data."""
-        return {
+        data: dict[str, Any] = {
             AZD_AVAILABLE: self.get_available(),
             AZD_CONNECTION_DATE: self.get_connection_date(),
             AZD_DISCONNECTION_DATE: self.get_disconnection_date(),
@@ -75,12 +90,29 @@ class WebServer:
             AZD_ID: self.get_id(),
             AZD_INSTALLATION: self.get_installation(),
             AZD_TYPE: self.get_type(),
-            AZD_WIFI_CHANNEL: self.get_wifi_channel(),
-            AZD_WIFI_MAC: self.get_wifi_mac(),
-            AZD_WIFI_QUALITY: self.get_wifi_quality(),
-            AZD_WIFI_RSSI: self.get_wifi_rssi(),
-            AZD_WIFI_SSID: self.get_wifi_ssid(),
         }
+
+        wifi_channel = self.get_wifi_channel()
+        if wifi_channel is not None:
+            data[AZD_WIFI_CHANNEL] = wifi_channel
+
+        wifi_mac = self.get_wifi_mac()
+        if wifi_mac is not None:
+            data[AZD_WIFI_MAC] = wifi_mac
+
+        wifi_quality = self.get_wifi_quality()
+        if wifi_quality is not None:
+            data[AZD_WIFI_QUALITY] = wifi_quality
+
+        wifi_rssi = self.get_wifi_rssi()
+        if wifi_rssi is not None:
+            data[AZD_WIFI_RSSI] = wifi_rssi
+
+        wifi_ssid = self.get_wifi_ssid()
+        if wifi_ssid is not None:
+            data[AZD_WIFI_SSID] = wifi_ssid
+
+        return data
 
     def get_available(self) -> bool:
         """Return availability status."""
